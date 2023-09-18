fa264c08dd9702c967ebaa85e54eb004418435a3
unknown <3160231312@qq.com>
2023-09-18 17:24:35 +0800

    perfect TestOffByOne.java

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -14,6 +14,7 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('&', '%'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'A'));
     }
 
 }
