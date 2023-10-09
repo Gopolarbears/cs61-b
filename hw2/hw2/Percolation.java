04b364481a2c2bf424cb9258f3d7f3328f2c08fa
unknown <3160231312@qq.com>
2023-10-09 15:42:58 +0800

    completed hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,85 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    int[] openGrid;
+    WeightedQuickUnionUF fullGrid;
+    int fullParent;
+    int size;
+    int openSites;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException("N must greater than or equal to 0");
+        }
+        openGrid = new int[N * N];
+        for (int i = 0; i < N; i ++) {
+            openGrid[i] = 1;
+        }
+        fullGrid = new WeightedQuickUnionUF(N * N);
+        size = N;
+        openSites = 0;
+    }
 
+    private void outOfIndex(int row, int col) {
+        if (row < 0 || col < 0 || row >= size || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException("the index out of range");
+        }
+    }
+
+    public void open(int row, int col) {
+        outOfIndex(row, col);
+        int location = row * size + col;
+        openGrid[location] = 1;
+        openWater(row, col);
+        openSites += 1;
+    }
+
+    private void openWater(int row, int col) {
+        int location = row * size + col;
+        if (openGrid[location - 1] == 1) {
+            fullGrid.union(location, location - 1);
+        }
+        if (openGrid[location + 1] == 1) {
+            fullGrid.union(location, location + 1);
+        }
+        if (row < size - 1) {
+            if (openGrid[location + size] == 1) {
+                fullGrid.union(location, location + size);
+            }
+        }
+        if (row > 0) {
+            if (openGrid[location - size] == 1) {
+                fullGrid.union(location, location - size);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        outOfIndex(row, col);
+        int location = row * size + col;
+        return openGrid[location] == 1;
+    }
+
+    public boolean isFull(int row, int col) {
+        outOfIndex(row, col);
+        int location = row * size + col;
+        for (int i = 0; i < size; i++) {
+            if (fullGrid.connected(i, location)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        for (int i = 0; i < size; i++) {
+            if (isFull(size - 1, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,45 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
+    double[] x;
+    double mean;
+    double stddev;
+    double doubleT;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException("index error");
+        }
+        doubleT = T;
+        Percolation oneTest;
+        x = new double[N];
+
+        for (int i = 0; i < T; i++) {
+            oneTest = pf.make(N);
+            while (!oneTest.percolates()) {
+                oneTest.open(StdRandom.uniform(0, N), StdRandom.uniform(0, N));
+            }
+            x[i] = oneTest.openSites / (double)(N * N);
+        }
+        mean = StdStats.mean(x);
+        stddev = StdStats.stddev(x);
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double stddev() {
+        return stddev;
+    }
+
+    public double confidenceLow() {
+        return (mean - 1.96 * stddev / Math.sqrt(doubleT));
+    }
+
+    public double confidenceHigh() {
+        return (mean + 1.96 * stddev / Math.sqrt(doubleT));
+    }
 
 }
