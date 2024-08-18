9ed2731bd45665c4af208067b61c85066f9156ae
buxiangqi <3160231312@qq.com>
2024-08-18 19:41:22 +0800

    hw5

---
+++ hw5/SeamCarver.java
@@ -0,0 +1,148 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+    Picture picture;
+    int[][] energy;
+    int[][] R;
+    int[][] G;
+    int[][] B;
+
+    public SeamCarver(Picture picture) {
+        this.picture = picture;
+        energy = new int[picture.width()][picture.height()];
+        rgbInitial();
+        energyInitial();
+    }
+
+    public Picture picture() {
+        return this.picture;
+    }
+
+    public int width() {
+        return this.picture.width();
+    }
+
+    public int height() {
+        return this.picture.height();
+    }
+
+    public double energy(int x, int y) {
+        return energy[x][y];
+    }
+
+    public int[] findHorizontalSeam() {
+        int[][] transportEnergy = new int[picture.height()][picture.width()];
+        for (int i = 0; i < width(); i++) {
+            for (int j = 0; j < height(); j++) {
+                transportEnergy[j][i] = energy[i][j];
+            }
+        }
+        int[][] temp = energy;
+        energy = transportEnergy;
+        int[] path = findVerticalSeam();
+        energy = temp;
+        return path;
+    }
+
+    public int[] findVerticalSeam() {
+        int[][] paths = new int[energy.length][energy[0].length];
+        int[][] M = new int[energy.length][energy[0].length];
+
+        for (int i = 0; i < energy.length; i++) {
+            M[i][0] = energy[i][0];
+        }
+
+        for (int j = 1; j < energy[0].length; j++) {
+            for (int i = 0; i < energy.length; i++) {
+                int leftM = (i - 1) < 0 ? Integer.MAX_VALUE : M[i - 1][j - 1];
+                int middleM = M[i][j - 1];
+                int rightM = (i + 1) >= energy.length ? Integer.MAX_VALUE : M[i + 1][j - 1];
+                int lastM = Math.min(leftM, Math.min(middleM, rightM));
+                M[i][j] = lastM + energy[i][j];
+                paths[i][j] = leftM == lastM ? i - 1 : middleM == lastM ? i : i + 1;
+            }
+        }
+
+        int lastLineM = Integer.MAX_VALUE;
+        int lastLineI = 0;
+        for (int i = 0; i < energy.length; i++) {
+            if (M[i][energy[0].length - 1] < lastLineM) {
+                lastLineM = M[i][energy[0].length - 1];
+                lastLineI = i;
+            }
+        }
+
+        int[] path = new int[energy[0].length];
+        path[energy[0].length - 1] = lastLineI;
+        for (int j = energy[0].length - 2; j >= 0; j--) {
+            path[j] = paths[path[j + 1]][j + 1];
+        }
+
+        return path;
+    }
+
+    public void removeHorizontalSeam(int[] seam) {
+        for (int i = 0; i < seam.length; i++) {
+            if (seam[i] < 0 || seam[i] >= height()) {
+                throw new IndexOutOfBoundsException(seam[i]);
+            }
+        }
+        if (seam.length != width()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < seam.length - 1; i++) {
+            if (Math.abs(seam[i] - seam[i + 1]) > 1) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void removeVerticalSeam(int[] seam) {
+        for (int i = 0; i < seam.length; i++) {
+            if (seam[i] < 0 || seam[i] >= width()) {
+                throw new IndexOutOfBoundsException(seam[i]);
+            }
+        }
+        if (seam.length != height()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < seam.length - 1; i++) {
+            if (Math.abs(seam[i] - seam[i + 1]) > 1) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void energyInitial() {
+        energy = new int[width()][height()];
+
+        for (int i = 0; i < picture.width(); i++) {
+            for (int j = 0; j < picture.height(); j++) {
+                int Rx = R[i - 1 < 0 ? width() - 1 : i - 1][j] - R[i + 1 >= picture.width() ? 0 : i + 1][j];
+                int Gx = G[i - 1 < 0 ? width() - 1 : i - 1][j] - G[i + 1 >= picture.width() ? 0 : i + 1][j];
+                int Bx = B[i - 1 < 0 ? width() - 1 : i - 1][j] - B[i + 1 >= picture.width() ? 0 : i + 1][j];
+                int Ry = R[i][j - 1 < 0 ? height() - 1 : j - 1] - R[i][j + 1 >= picture.height() ? 0 : j + 1];
+                int Gy = G[i][j - 1 < 0 ? height() - 1 : j - 1] - G[i][j + 1 >= picture.height() ? 0 : j + 1];
+                int By = B[i][j - 1 < 0 ? height() - 1 : j - 1] - B[i][j + 1 >= picture.height() ? 0 : j + 1];
+                energy[i][j] = Rx * Rx + Gx * Gx + Bx * Bx + Ry * Ry + Gy * Gy + By * By;
+            }
+        }
+    }
+
+    private void rgbInitial() {
+        R = new int[width()][height()];
+        G = new int[width()][height()];
+        B = new int[width()][height()];
+
+        for (int i = 0; i < picture.width(); i++) {
+            for (int j = 0; j < picture.height(); j++) {
+                int rgb = picture.getRGB(i, j);
+                R[i][j] = (rgb >> 16) & 0xff;
+                G[i][j] = (rgb >> 8) & 0xff;
+                B[i][j] = rgb & 0xff;
+            }
+        }
+    }
+}
